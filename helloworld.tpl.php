5c6984fa8fa8b0709eb31c4d8908b85a59f73041
tricia jose <tricia@appnovation.com>
2013-02-06 09:37:02 -0800

    added theme

---
+++ helloworld.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+// $node = $variables['message'];
+// $build = node_view($node);
+
+// $output = drupal_render($build);
+
+// return <h1> $output</h1>;
+
+
+ ?>
+
+
+<div style= "color:#0000FF">
+
+ Hello <?php print $name ?>
+
+</div>
+
+
+
+
